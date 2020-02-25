a65328c8e1e91576d1e5edb6633f29d06bdcb7f9
Alberto Quattrini Li <alberto.quattrini.li@dartmouth.edu>
2020-02-24 23:47:07 -0500

    check with Cartesian coordinates instead of polar coordinates.

--- scripts/scan_filter.py
+++ scripts/scan_filter.py
@@ -36,13 +36,13 @@ class ScanFilter:
         ats = ApproximateTimeSynchronizer([self.base_pose_subscriber, self.scan_subscriber], 10, 0.1)
         ats.registerCallback(self.topic_callback)
 
-    def process_scan_message(self, msg, robot_ranges, robot_angles):
+    def process_scan_message(self, msg, robot_poses):#robot_ranges, robot_angles): # TODO cleanup the code.
         angle_min = msg.angle_min
         angle_increment = msg.angle_increment
         range_min = msg.range_min
         range_max = msg.range_max
         ranges = msg.ranges
-        robot_count = len(robot_ranges)
+        robot_count = len(robot_poses)#ranges)
         size = len(ranges)
         new_ranges = [0.0] * size
         for i in range(size):
@@ -50,9 +50,13 @@ class ScanFilter:
                 r = ranges[i]
                 angle = angle_min + i * angle_increment
                 is_robot = False
+                r_x, r_y = r * np.cos(angle), r * np.sin(angle)
                 for j in range(robot_count):
+                    """
                     if (abs(robot_ranges[j] - r) < self.map_inflation_radius) and (
-                            abs(robot_angles[j] - angle) < self.map_inflation_radius):
+                            abs(robot_angles[j] - angle) < 0.6): # TODO angle based on distance.
+                    """
+                    if abs(r_x - robot_poses[j][0]) < self.map_inflation_radius and abs(r_y - robot_poses[j][1]):
                         is_robot = True
                         break
                 if is_robot:
@@ -83,12 +87,13 @@ class ScanFilter:
                 pose_msg.pose.pose.orientation.w))
             scan_time = scan_msg.scan_time  # time (sec) between scans
             msg_time = scan_msg.header.stamp.to_sec()
-            robot_ranges, robot_angles = self.find_other_robots(robot_pose, msg_time, scan_time)
-            self.process_scan_message(scan_msg, robot_ranges, robot_angles)
+            robot_poses = self.find_other_robots(robot_pose, msg_time, scan_time) #robot_ranges, robot_angles
+            self.process_scan_message(scan_msg, robot_poses) #, robot_ranges, robot_angles)
 
     def find_other_robots(self, robot_pose, msg_time, scan_time):
         robot_ranges = []
         robot_angles = []
+        robot_poses = []
         mTr, rTm = self.generate_transformation_matrices(robot_pose)
         robot_ids = list(self.all_poses)
         for rid in robot_ids:
@@ -101,9 +106,10 @@ class ScanFilter:
                 angle = tf.transformations.euler_from_matrix(rTo[0:3,0:3])[2]
                 robot_ranges.append(distance)
                 robot_angles.append(angle)
+                robot_poses.append((rTo[0,3], rTo[1,3]))
             else:
                 rospy.logerr("difference {} {}".format(t - msg_time, scan_time))
-        return robot_ranges, robot_angles
+        return robot_poses#robot_ranges, robot_angles
 
     def generate_transformation_matrices(self, pose):
         mTr = tf.transformations.quaternion_matrix(pose[2])
