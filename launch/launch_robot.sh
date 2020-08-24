a49143328111e4099904e74003a8edc105b7dc79
Kizito <kmasaba21@gmail.com>
2020-08-24 02:57:55 +0000

    server updates

--- launch/launch_robot.sh
+++ launch/launch_robot.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-nohup sudo airmon-ng start wlan1 &
-sudo su -c ./launch_script.sh root
-nohup roslaunch gvgexploration husarion.launch robot_id:=4 base_station:=5,7,8 robot_type:=2 &
+roslaunch gvgexploration husarion.launch robot_id:=4 base_station:=5,7,8 robot_type:=2
