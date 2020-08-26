bdca067a3fe8ccc232cce3cabed7e7eff8f84ad2
lec9243 <lec9243@gmail.com>
2020-08-26 04:15:28 +0000

    local changes

--- scripts/robot.py
+++ scripts/robot.py
@@ -563,7 +563,7 @@ class Robot:
                         self.handle_intersection(close_devices)
                 else:
                     pu.log_msg(self.robot_id, "Waiting for frontier points...", self.debug_mode)
-
+ 
     def start_exploration_action(self, frontier_ridge):
         while self.map_updating:  # wait for map to update
             sleep(1)
