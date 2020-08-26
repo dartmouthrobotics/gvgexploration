3b49d6fb3192f5fba26b56b91aea3b023a352e0f
John MacDonald <john.l.macdonald.22@dartmouth.edu>
2020-08-26 04:49:29 +0000

    local chances

--- launch/launch_robot4.sh
+++ launch/launch_robot4.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+sudo sh -c "echo 1 >/proc/sys/net/ipv4/ip_forward"
+sudo sh -c "echo 0 >/proc/sys/net/ipv4/icmp_echo_ignore_broadcasts"
+
 roslaunch gvgexploration husarion.launch robot_id:=4 base_station:=5,7,8 robot_type:=2
