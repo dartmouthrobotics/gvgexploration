7557454d0e8ff0d1b53440ce5b4c635c0dd1e3ec
Alberto Quattrini Li <alberto.quattrini.li@dartmouth.edu>
2020-12-06 02:40:04 -0500

    added the loop for target_ratio if recurrent_connectivity

--- scripts/run_batch.py
+++ scripts/run_batch.py
@@ -12,7 +12,7 @@ import rospkg
 
 nrobots_all =[2] #[2, 4, 6]
 
-methods = ["continuous_connectivity"] #"gvgexploration","recurrent_connectivity",
+methods = ["continuous_connectivity", "recurrent_connectivity"] #"gvgexploration","recurrent_connectivity",
 runs = [0,1,2,3,4]
 envs = {"office": [33.0, 20.0],"cave": [20.0, 8.0],"city": [25.0, 4.0]}
 target_ratios=[0.05,0.5]
@@ -33,21 +33,25 @@ def check_kill_process(pstring):
         print(line)
         os.kill(int(pid), signal.SIGKILL)
 
+def start_simulation(launcher_args):
+    errors_before = num_errors()
+    rospy.logerr(launcher_args)
+    main_process = subprocess.Popen(launcher_args)
+    main_process.wait()
+    check_kill_process("ros")
+    time.sleep(10)
+
+
 for nrobots in nrobots_all:
     for package in methods:
+        i = 0
         for run in runs:
             for world, bs_pose in envs.items():
-           
-                errors_before = num_errors()
-                launcher_args = ['roslaunch', package, 'multirobot_{}.launch'.format(nrobots), "world:={}{}".format(world,nrobots) , "robot_count:={}".format(nrobots),"run:={}".format(run),"method:={}".format(package),"environment:={}".format(world),"bs_pose:={},{}".format(bs_pose[0],bs_pose[1]),"max_target_info_ratio:={}".format(0.05)]
-                rospy.logerr(launcher_args)
-                main_process = subprocess.Popen(launcher_args)
-                main_process.wait()
-                check_kill_process("ros")
-                time.sleep(10)
+                launcher_args = ['roslaunch', package, 'multirobot_{}.launch'.format(nrobots), "world:={}{}".format(world,nrobots) , "robot_count:={}".format(nrobots),"run:={}".format(run),"method:={}".format(package),"environment:={}".format(world),"bs_pose:={},{}".format(bs_pose[0],bs_pose[1])]
+                launcher_args.append("max_target_info_ratio:={}".format(target_ratios[i]))
                 if package=='recurrent_connectivity':
-                    launcher_args = ['roslaunch', package, 'multirobot_{}.launch'.format(nrobots), "world:={}{}".format(world,nrobots) , "robot_count:={}".format(nrobots),"run:={}".format(run),"method:={}".format(package),"environment:={}".format(world),"bs_pose:={},{}".format(bs_pose[0],bs_pose[1]),"max_target_info_ratio:={}".format(0.5)]
-                    rospy.logerr(launcher_args)
-                    main_process = subprocess.Popen(launcher_args)
-                    main_process.wait()
-                    check_kill_process("ros")
+                    for t in target_ratios:
+                        launcher_args[-1] = "max_target_info_ratio:={}".format(target_ratios[i])
+                        start_simulation(launcher_args)
+                else:
+                    start_simulation(launcher_args)
