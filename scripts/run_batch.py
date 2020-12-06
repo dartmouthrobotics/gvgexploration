6e39074504a6b8e9060598ae368eab64460f9d0e
kmasaba21 <kmasaba21@gmail.com>
2020-12-05 22:37:50 -0500

    got some updates

--- scripts/run_batch.py
+++ scripts/run_batch.py
@@ -11,9 +11,10 @@ import rosgraph
 import rospkg
 
 nrobots_all =[2] #[2, 4, 6]
-methods = ["gvgexploration"]  #["gvgexploration","continuous_connectivity","recurrent_connectivity"]
-runs = [0]#,1,2,3,4]
-envs = {"office": [33.0, 20.0]} #,"cave": [20.0, 8.0],
+
+methods = ["continuous_connectivity"] #"gvgexploration","recurrent_connectivity",
+runs = [0,1,2,3,4]
+envs = {"office": [33.0, 20.0],"cave": [20.0, 8.0],"city": [25.0, 4.0]}
 target_ratios=[0.05,0.5]
 rospack = rospkg.RosPack()
 package_path = rospack.get_path('gvgexploration') + "/log/errors.log" 
@@ -43,11 +44,10 @@ for nrobots in nrobots_all:
                 main_process = subprocess.Popen(launcher_args)
                 main_process.wait()
                 check_kill_process("ros")
-                time.sleep(10) 
+                time.sleep(10)
                 if package=='recurrent_connectivity':
                     launcher_args = ['roslaunch', package, 'multirobot_{}.launch'.format(nrobots), "world:={}{}".format(world,nrobots) , "robot_count:={}".format(nrobots),"run:={}".format(run),"method:={}".format(package),"environment:={}".format(world),"bs_pose:={},{}".format(bs_pose[0],bs_pose[1]),"max_target_info_ratio:={}".format(0.5)]
                     rospy.logerr(launcher_args)
                     main_process = subprocess.Popen(launcher_args)
                     main_process.wait()
                     check_kill_process("ros")
-
